91c70938f7fa3dbf209055a794fa857c51603c88
Shunyo Kawamoto <syunyo1028@yahoo.co.jp>
2023-01-28 03:58:09 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Python Development Template
+
+This is a template for Python development on Docker with VSCode.
+
+You can customize the details according to your own preference.
+
+## Feature
+
+- Docker
+  - Python 3.10
+  - Support for both CPU and GPU
+- Package management with Poetry
+- VScode
+  - Optimized setup for Python development
--- notebooks/init.py
+++ notebooks/init.py
@@ -1,35 +1,5 @@
-import os
-import subprocess
 import sys
-
-import pandas as pd
-from IPython import get_ipython
-from tqdm.auto import tqdm
-
-os.environ["MPLCONFIGDIR"] = "/tmp/matplotlib"
-
-
-# -- pandas --
-pd.set_option("display.max_colwidth", 500)
-pd.set_option("display.max_rows", 1000)
 
 # -- add path --
 
 sys.path.append("../")
-
-# -- tqdm --
-
-tqdm.pandas(desc="Processing")
-
-# -- magic command --
-
-ipython = get_ipython()
-if ipython:
-    running_in_notebook = ipython.has_trait("kernel")
-    if running_in_notebook:
-        ipython.magic("reload_ext autoreload")
-        ipython.magic("autoreload 2")
-        ipython.magic("matplotlib inline")
-else:
-    # cannot even get ipython object...
-    running_in_notebook = False
