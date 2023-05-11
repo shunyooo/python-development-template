eac2b58282f9743b53ec322d45a62a79889a5356
Shunyo Kawamoto <syunyo1028@yahoo.co.jp>
2023-05-11 15:06:54 +0900

    refactor: postCreateCommand install make, gcloud

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+# poetry
+export POETRY_VIRTUALENVS_CREATE=true
+poetry install
+
+# install make
+apt-get update
+apt-get install make
+
+# install gcloud
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-cli -y
